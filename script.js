1f06ca8c523cd98e39fa006e3b7e9ad127d7efdc
Nathaniel Tang <nathanieltang22@gmail.com>
2022-05-11 18:18:24 -0400

    added code to mark gameSquares, and updated the render function to correctly delete previous render

--- script.js
+++ script.js
@@ -1,48 +1,83 @@
-const gameboard = (()=>{
-    let gameboardArray = [1,2,3,4,5,6,7,8,9]
-        //nine objects, with Status: X, O, or empty
-        //then the other functions will manipulate the objects within gameboardArray to change the Status property
-    let gameboardSpace = document.querySelector('#gameboardSpace')
-    const _createGameboard = () => {
-        gameboardArray.forEach(element => {
-            let gameboardSquare = document.createElement('div');
-            gameboardSquare.textContent = "X";
+const gameboard = (() => {
+  let gameboardArray = [
+      {status: "empty"},
+      {status: "empty"},
+      {status: "empty"},
+      {status: "empty"},
+      {status: "empty"},
+      {status: "empty"},
+      {status: "empty"},
+      {status: "empty"},
+      {status: "empty"},
 
-            gameboardSpace.appendChild(gameboardSquare)
-        }); 
-    } 
+  ];
 
+ 
+  let gameboardSpace = document.querySelector("#gameboardSpace");
+  const _createGameboard = () => {
+    gameboardSpace.innerHTML = "";
+    gameboardArray.forEach((element, index) => {
+      let gameboardSquare = document.createElement("div");
+      gameboardSquare.classList.add("gameboard");
+      gameboardSquare.dataset.index = index;
 
-    const render = () => {
-        _createGameboard()
+      if (element.status === "empty") {
+        gameboardSquare.textContent = "";
+      } else if (element.status === "X") {
+        gameboardSquare.textContent = "X";
+      } else if (element.status === "O") {
+        gameboardSquare.textContent = "O";
+      } else return;
+      gameboardSpace.appendChild(gameboardSquare);
+
+      gameboardSquare.addEventListener("click", markSpace);
+    });
+  };
+
+  const markSpace = function markSpace() {
+    //where mark is X or O;
+    let index = this.dataset.index;
+    console.log("a" + this.dataset.index);
+    mark = "X";
+    if (mark === "X") {
+      gameboardArray[index].status = "X";
+    } else if (mark === "O") {
+      gameboardArray[index].status = "O";
     }
-    return {render} //don't return createGameboard
+    render();
+    console.log(this);
+  };
+  const render = () => {
+    _createGameboard();
+  };
+  return {render, markSpace}; //don't return createGameboard
 })();
 
-const gameFlow = (() => {
-})();
+gameboard.render();
+
+const gameFlow = (() => {})();
 
 const player = (name) => {
-    const sayName = () => {
-        console.log(name)
-    }
-    return {
-        name,
-        sayName
-    }
-}
+  const sayName = () => {
+    console.log(name);
+  };
+  return {
+    name,
+    sayName,
+  };
+};
 
 const addPlayer = () => {
-    //add name from HTML and assign X or O
-    //get DOM
-    let getName = document.querySelector('#playerName');
-    let name = getName.textContent;
+  //add name from HTML and assign X or O
+  //get DOM
+  let getName = document.querySelector("#playerName");
+  let name = getName.textContent;
 
-    console.log(name)
+  console.log(name);
 
-    player(name)
-}
+  player(name);
+};
 
-function test() {
-    gameboard.render();
-}
+const test = function test() {
+  gameboard.render();
+};
